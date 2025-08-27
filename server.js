af796337ca064d91699bdb4e7386a664b63cdf10
Gaurav Singh <gaura@MacBook-Air.local>
2025-08-27 15:27:34 +0530

    Your commit message

--- server.js
+++ server.js
@@ -28,3 +28,21 @@ const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+    // {
+    //   "version": 2,
+    //   "builds": [
+    //     {
+    //       "src": "./server.js",
+    //       "use": "@vercel/node"
+    //     }
+    //   ],
+    //   "routes": [
+    //     {
+    //       "src": "/(.*)",
+    //       "dest": "/api"
+    //     }
+    //   ]
+    // }
